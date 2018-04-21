9828b05d1537b88d9755689d5073f1cb807f22f6
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-04-21 10:52:20 -0300

    Agrego comandos builtin

--- skel/builtin.c
+++ skel/builtin.c
@@ -1,12 +1,19 @@
 #include "builtin.h"
+#include "defs.h"
 
 // returns true if the 'exit' call
 // should be performed
 int exit_shell(char* cmd) {
+    char command[ARGSIZE];
+    if (sscanf(cmd, "%s", command) < 0) {
+        return 0;
+    }
 
-	// Your code here 
+    if (strncmp(command, "exit", ARGSIZE) != 0) {
+        return 0;
+    }
 
-	return 0;
+    return 1;
 }
 
 // returns true if "chdir" was performed
@@ -15,18 +22,54 @@ int exit_shell(char* cmd) {
 // 	$ cd (change to HOME)
 // it has to be executed and then return true
 int cd(char* cmd) {
-	
-	// Your code here
+    char command[ARGSIZE];
+    char dir[BUFLEN] = {0};
+    if (sscanf(cmd, "%s %s", command, dir) < 0) {
+        perror("cd sscanf");
+        return 0;
+    }
 
-	return 0;
+    if (strncmp(command, "cd", ARGSIZE) != 0) { // not cd
+        return 0;
+    }
+
+    if (!strlen(dir)) {
+        memcpy(dir, getenv("HOME"), ARGSIZE);
+    }
+
+	if (chdir(dir) < 0) {
+        perror("cd chdir");
+        return 0;
+    }
+
+    if (getcwd(dir, BUFLEN) < 0) {
+        perror("cd getcwd");
+        return 0;
+    }
+
+    snprintf(promt, PRMTLEN, "(%s)", dir);
+	return 1;
 }
 
 // returns true if 'pwd' was invoked
 // in the command line
 int pwd(char* cmd) {
+    char command[ARGSIZE];
+    if (sscanf(cmd, "%s", command) < 0) {
+        perror("pwd sscanf");
+        return 0;
+    }
 
-	// Your code here
-	
-	return 0;
+    if (strncmp(command, "pwd", ARGSIZE) != 0) {
+        return 0;
+    }
+
+    char wd[BUFLEN];
+    if (getcwd(wd, BUFLEN) < 0) {
+        perror("cd getcwd");
+        return 0;
+    }
+    printf("%s\n", wd);
+    return 1;
 }
 
